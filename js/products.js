25a036d89848bf789ee666f3b6635ab413d9c815
Rokas <rokas.kmaro@gmail.com>
2023-12-21 18:21:32 +0200

    landing(master) testing

--- js/products.js
+++ js/products.js
@@ -1,67 +1,113 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const productSlider = document.getElementById('products-slider');
     const productBlock = document.getElementById('products');
-    const productSliderMobile = document.getElementById('products-slider-mobile');
-    const productsMaterial = document.getElementById('products-material');
-    const productsManufactures = document.getElementById('products-manufactures');
+    const productMobileButtons = document.querySelectorAll('[data-products-mobile-target]');
 
     const productButtons = document.querySelectorAll('[data-products-target]');
-    const products = document.querySelectorAll('[data-products]');
     let activeSliderIndex = 0;
+    const isMobile = window.matchMedia('(max-width: 1024px)').matches;
 
-    function removeActiveButtons() {
-        productButtons.forEach(button => {
-            button.classList.remove('active-product')
-        })
-    }
+    // desktop slider
+    console.log(isMobile)
 
-    let slider = new window.Splide('#products-slider', {
-        arrows: true,
-        pagination: true,
-        type: "loop",
-        gap: '87px',
-        classes: {
-            page: 'splide__pagination__page custom-pagination-item'
-        }
-    });
-    let heightMap = {};
-    document.querySelectorAll(".splide__slide").forEach(function (e) {
-        e.style.maxHeight = 0;
-    });
-    slider.on("mounted", function () {
-        let i = 0;
-        document.querySelectorAll(".splide__slide").forEach(function (e) {
-            if (!e.classList.contains('splide__slide--clone')) {
-                heightMap[i] = e.scrollHeight;
-                i++
+    if (!isMobile) {
+        let slider = new window.Splide('#products-slider', {
+            arrows: true,
+            pagination: true,
+            type: "loop",
+            gap: '87px',
+            classes: {
+                page: 'splide__pagination__page custom-pagination-item'
             }
         });
-    });
-    slider.on("active", function (e) {
-        // Or index if not loop
-        // let maxHeight = heightMap[e.index] + "px";
-        let maxHeight = heightMap[e.slideIndex] + "px";
-        e.slide.style.maxHeight = maxHeight;
-        e.slide.parentElement.style.maxHeight = maxHeight;
-    });
+        let heightMap = {};
+        document.querySelectorAll(".splide__slide").forEach(function (e) {
+            e.style.maxHeight = 0;
+        });
+        slider.on("mounted", function () {
+            let i = 0;
+            document.querySelectorAll(".splide__slide").forEach(function (e) {
+                if (!e.classList.contains('splide__slide--clone')) {
+                    heightMap[i] = e.scrollHeight;
+                    i++
+                }
+            });
+        });
+        slider.on("active", function (e) {
+            // Or index if not loop
+            // let maxHeight = heightMap[e.index] + "px";
+            let maxHeight = heightMap[e.slideIndex] + "px";
+            e.slide.style.maxHeight = maxHeight;
+            e.slide.parentElement.style.maxHeight = maxHeight;
+        });
 
-    slider.on("move", function (e) {
-        activeSliderIndex = e
-        changeActiveButton()
-        productBlock.scrollIntoView()
-    })
-
-    productButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            removeActiveButtons()
-            const selectedProduct = button.dataset.productsTarget
-            button.classList.add('active-product')
-            slider.go(parseInt(selectedProduct))
+        slider.on("move", function (e) {
+            activeSliderIndex = e
+            changeActiveButton(productButtons)
+            productBlock.scrollIntoView()
         })
-    })
 
-    function changeActiveButton() {
         productButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                removeActiveButtons(productButtons)
+                const selectedProduct = button.dataset.productsTarget
+                button.classList.add('active-product')
+                slider.go(parseInt(selectedProduct))
+            })
+        })
+
+        slider.mount();
+
+    } else {
+        // mobile slider
+        let sliderMobile = new Splide('#products-slider-mobile', {
+            perPage: 0,
+            focus: 0,
+            type: "loop",
+            pagination: true,
+            gap: '87px',
+            breakpoints: {
+                1024: {
+                    perPage: 3,
+                },
+                768: {
+                    perPage: 2,
+                },
+                500: {
+                    perPage: 1,
+                }
+            },
+            classes: {
+                page: 'splide__pagination__page custom-pagination-item'
+            }
+        })
+
+        sliderMobile.on("move", function (e) {
+            activeSliderIndex = e
+
+            productMobileButtons.forEach(button => {
+                const selectedProduct = button.dataset.productsMobileTarget.split(',')
+                if (selectedProduct.includes(activeSliderIndex.toString())) {
+                    button.classList.add('active-product')
+                } else {
+                    button.classList.remove('active-product')
+                }
+            })
+        })
+
+        productMobileButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                removeActiveButtons(productMobileButtons)
+                const selectedProduct = button.dataset.productsMobileTarget.split(',')
+                button.classList.add('active-product')
+                sliderMobile.go(parseInt(selectedProduct[0]))
+            })
+        })
+
+        sliderMobile.mount();
+    }
+
+    function changeActiveButton(list) {
+        list.forEach(button => {
             if (parseInt(button.dataset.productsTarget) === activeSliderIndex) {
                 button.classList.add('active-product')
             } else {
@@ -70,90 +116,9 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     }
 
-
-    slider.mount();
-});
-
-
-// productSlider && new Splide('#products-slider', {
-//     perPage: 1,
-//     focus: 0,
-//     type: "loop",
-//     pagination: true,
-//     gap: '87px',
-//     autoHeight: true,
-//     breakpoints: {
-//         1024: {
-//             perPage: 0,
-//         }
-//     },
-//     classes: {
-//         page: 'splide__pagination__page custom-pagination-item'
-//     }
-// }).mount();
-
-// productSliderMobile && new Splide('#products-slider-mobile', {
-//     perPage: 0,
-//     focus: 0,
-//     type: "loop",
-//     pagination: true,
-//     gap: '87px',
-//     breakpoints: {
-//         1024: {
-//             perPage: 3,
-//         },
-//         768: {
-//             perPage: 2,
-//         },
-//         500: {
-//             perPage: 1,
-//         }
-//     },
-//     classes: {
-//         page: 'splide__pagination__page custom-pagination-item'
-//     }
-// }).mount();
-//
-// productsMaterial && new Splide('#products-material', {
-//     perPage: 3,
-//     focus: 0,
-//     type: "loop",
-//     pagination: true,
-//     gap: '87px',
-//     drag: false,
-//     dragAngleThreshold: 0,
-//     breakpoints: {
-//         768: {
-//             perPage: 2,
-//             drag: true,
-//         },
-//         500: {
-//             perPage: 1,
-//         }
-//     },
-//     classes: {
-//         page: 'splide__pagination__page custom-pagination-item'
-//     }
-// }).mount();
-//
-// productsManufactures && new Splide('#products-manufactures', {
-//     perPage: 3,
-//     focus: 0,
-//     type: "loop",
-//     pagination: true,
-//     gap: '87px',
-//     drag: false,
-//     dragAngleThreshold: 0,
-//     breakpoints: {
-//         768: {
-//             perPage: 2,
-//             drag: true,
-//         },
-//         500: {
-//             perPage: 1,
-//         }
-//     },
-//     classes: {
-//         page: 'splide__pagination__page custom-pagination-item'
-//     }
-// }).mount();
+    function removeActiveButtons(list) {
+        list.forEach(button => {
+            button.classList.remove('active-product')
+        })
+    }
+})
