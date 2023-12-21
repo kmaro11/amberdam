09b32b19f9efba153513cf07efb0835e66b074b4
Rokas <rokas.kmaro@gmail.com>
2023-12-21 16:13:04 +0200

    landing(master) testing

--- js/products.js
+++ js/products.js
@@ -29,87 +29,113 @@ function removeActiveButtons() {
     })
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    let slider = new window.Splide('#products-slider', { arrows: false, pagination: false, type: "loop", gap: '87px'});
+    let heightMap = {};
+    document.querySelectorAll(".splide__slide").forEach(function (e) {
+        e.style.maxHeight = 0;
+    });
+    slider.on("mounted", function () {
+        let i = 0;
+        document.querySelectorAll(".splide__slide").forEach(function (e) {
+            if (!e.classList.contains('splide__slide--clone')) {
+                heightMap[i] = e.scrollHeight;
+                i++
+            }
+        });
+    });
+    slider.on("active", function (e) {
+        // Or index if not loop
+        // let maxHeight = heightMap[e.index] + "px";
+        let maxHeight = heightMap[e.slideIndex] + "px";
+        e.slide.style.maxHeight = maxHeight;
+        e.slide.parentElement.style.maxHeight = maxHeight;
+    });
+    slider.mount();
+});
 
 
 
-productSlider && new Splide('#products-slider', {
-    perPage: 1,
-    focus: 0,
-    type: "loop",
-    pagination: true,
-    gap: '87px',
-    breakpoints: {
-        1024: {
-            perPage: 0,
-        }
-    },
-    classes: {
-        page: 'splide__pagination__page custom-pagination-item'
-    }
-}).mount();
 
-productSliderMobile && new Splide('#products-slider-mobile', {
-    perPage: 0,
-    focus: 0,
-    type: "loop",
-    pagination: true,
-    gap: '87px',
-    breakpoints: {
-        1024: {
-            perPage: 3,
-        },
-        768: {
-            perPage: 2,
-        },
-        500: {
-            perPage: 1,
-        }
-    },
-    classes: {
-        page: 'splide__pagination__page custom-pagination-item'
-    }
-}).mount();
+// productSlider && new Splide('#products-slider', {
+//     perPage: 1,
+//     focus: 0,
+//     type: "loop",
+//     pagination: true,
+//     gap: '87px',
+//     autoHeight: true,
+//     breakpoints: {
+//         1024: {
+//             perPage: 0,
+//         }
+//     },
+//     classes: {
+//         page: 'splide__pagination__page custom-pagination-item'
+//     }
+// }).mount();
 
-productsMaterial && new Splide('#products-material', {
-    perPage: 3,
-    focus: 0,
-    type: "loop",
-    pagination: true,
-    gap: '87px',
-    drag: false,
-    dragAngleThreshold: 0,
-    breakpoints: {
-        768: {
-            perPage: 2,
-            drag: true,
-        },
-        500: {
-            perPage: 1,
-        }
-    },
-    classes: {
-        page: 'splide__pagination__page custom-pagination-item'
-    }
-}).mount();
-
-productsManufactures && new Splide('#products-manufactures', {
-    perPage: 3,
-    focus: 0,
-    type: "loop",
-    pagination: true,
-    gap: '87px',
-    drag: false,
-    dragAngleThreshold: 0,
-    breakpoints: {
-        768: {
-            perPage: 2,
-            drag: true,
-        },
-        500: {
-            perPage: 1,
-        }
-    },
-    classes: {
-        page: 'splide__pagination__page custom-pagination-item'
-    }
-}).mount();
+// productSliderMobile && new Splide('#products-slider-mobile', {
+//     perPage: 0,
+//     focus: 0,
+//     type: "loop",
+//     pagination: true,
+//     gap: '87px',
+//     breakpoints: {
+//         1024: {
+//             perPage: 3,
+//         },
+//         768: {
+//             perPage: 2,
+//         },
+//         500: {
+//             perPage: 1,
+//         }
+//     },
+//     classes: {
+//         page: 'splide__pagination__page custom-pagination-item'
+//     }
+// }).mount();
+//
+// productsMaterial && new Splide('#products-material', {
+//     perPage: 3,
+//     focus: 0,
+//     type: "loop",
+//     pagination: true,
+//     gap: '87px',
+//     drag: false,
+//     dragAngleThreshold: 0,
+//     breakpoints: {
+//         768: {
+//             perPage: 2,
+//             drag: true,
+//         },
+//         500: {
+//             perPage: 1,
+//         }
+//     },
+//     classes: {
+//         page: 'splide__pagination__page custom-pagination-item'
+//     }
+// }).mount();
+//
+// productsManufactures && new Splide('#products-manufactures', {
+//     perPage: 3,
+//     focus: 0,
+//     type: "loop",
+//     pagination: true,
+//     gap: '87px',
+//     drag: false,
+//     dragAngleThreshold: 0,
+//     breakpoints: {
+//         768: {
+//             perPage: 2,
+//             drag: true,
+//         },
+//         500: {
+//             perPage: 1,
+//         }
+//     },
+//     classes: {
+//         page: 'splide__pagination__page custom-pagination-item'
+//     }
+// }).mount();
